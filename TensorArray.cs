012f3695367d14ec300d5162ffe6708e467fc85b
Vasily42 <vmbat2004@gmail.com>
2021-11-14 20:07:18 +0000

    fix: vasya's brain added

--- TensorArray.cs
+++ TensorArray.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
 namespace NeuralNetwork
